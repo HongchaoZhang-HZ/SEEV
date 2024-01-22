e50dfde6cd890ede3d81191c02949991943da3fa
hongchao <hongchao@wustl.edu>
2024-01-22 17:24:26 -0600

    Delete BaBBasic.py script

---
+++ Phase1_Scalability/Scripts/NSBasic.py
@@ -0,0 +1,6 @@
+import sys, os
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+from Modules.utils import *
+from Modules.NNet import NeuralNetwork as NNet
+from Status import NeuronStatus, NetworkStatus
+
--- Phase1_Scalability/test/test_BaBBasic.py
+++ Phase1_Scalability/test/test_BaBBasic.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 import unittest
 from Modules.utils import *
 from Modules.NNet import NeuralNetwork as NNet
-from Scripts.BaBBasic import NStatus, NeuronStatus, NetworkStatus
+from Phase1_Scalability.Scripts.Status import NeuronStatus, NetworkStatus
 
 class TestNeuronStatus(unittest.TestCase):
     def test_init(self):
