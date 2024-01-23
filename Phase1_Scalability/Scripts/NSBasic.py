079cf19e7064983d56669e669fbe822ec87e070e
hongchao <hongchao@wustl.edu>
2024-01-22 21:18:40 -0600

    Refactor NSBasic class and add properties

--- Phase1_Scalability/Scripts/NSBasic.py
+++ Phase1_Scalability/Scripts/NSBasic.py
@@ -4,8 +4,41 @@ from Modules.utils import *
 from Modules.NNet import NeuralNetwork as NNet
 from Status import NeuronStatus, NetworkStatus
 
-class NSBasic(NetworkStatus):
-    def __init__(self, network):
-        super().__init__(network)
+class NSBasic:
+    def __init__(self):
+        self.set_P = {}
+        self.set_N = {}
+        self.set_Z = {}
+        self.set_U = {}
         
+    @property
+    def set_P(self):
+        return self.set_P
+        
+    @property
+    def set_N(self):
+        return self.set_N
     
+    @property
+    def set_Z(self):
+        return self.set_Z
+    
+    @property
+    def set_U(self):
+        return self.set_U
+    
+    @set_P.setter
+    def set_P(self, set_P):
+        self.set_P = set_P
+    
+    @set_N.setter
+    def set_N(self, set_N):
+        self.set_N = set_N
+        
+    @set_Z.setter
+    def set_Z(self, set_Z):
+        self.set_Z = set_Z
+        
+    @set_U.setter
+    def set_U(self, set_U):
+        self.set_U = set_U
