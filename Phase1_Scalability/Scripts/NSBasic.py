0969bd889a85a1a61a43a394b75011933b31b7b8
hongchao <hongchao@wustl.edu>
2024-01-25 22:32:03 -0600

    Add NeuralNetwork import to NSBasic.py

--- Phase1_Scalability/Scripts/NSBasic.py
+++ Phase1_Scalability/Scripts/NSBasic.py
@@ -2,6 +2,7 @@ import re
 import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from Modules.utils import *
+from Modules.NNet import NeuralNetwork as NNet
 from Scripts.Status import NeuronStatus, NetworkStatus
 
 class NSBasic:
