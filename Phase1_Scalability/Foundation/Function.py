74dfe068d37dc48fc60dfa00fdc56c789fc3b5aa
hongchao <hongchao@wustl.edu>
2024-03-27 19:43:31 -0500

    Refactor RoA function and add solver_lp function

--- Phase1_Scalability/Foundation/Function.py
+++ Phase1_Scalability/Foundation/Function.py
@@ -10,55 +10,32 @@ from Scripts.Status import NeuronStatus, NetworkStatus
 # return a set of linear constraints to formulate $\mathcal{X}(S)$
 
 # Region of Activation (RoA) is the set of points that are activated by a ReLU NN
-def RoA(S:dict, prog:MathematicalProgram, x):
-    # Initialize the set of constraints
-    X = []
-    
-    W, r = LinearExp(S)
-    
-    # Return the set of constraints
-    return prog
-
-def activated_weight_bias_ml(model,activated_set,num_neuron):
-    W_list = []
-    r_list = []
-    para_list = list(model.state_dict())
-    i = 0
-    while i < (len(para_list)):
-        weight = model.state_dict()[para_list[i]]
-        i += 1
-        bias = model.state_dict()[para_list[i]]
-        i += 1
-        W_list.append(weight)
-        r_list.append(bias)
-    # compute the activated weight of the layer
-    for l in range(2):
-        # compute region/boundary weight
-
-
-        if l == 0:
-            W_l = torch.mul(activated_set[num_neuron*l:num_neuron*(l+1)], W_list[l])
-            r_l = torch.mul(activated_set[num_neuron*l:num_neuron*(l+1)], torch.reshape(r_list[l], [len(r_list[l]), 1]))
-            W_a = W_l
-            r_a = r_l
-            W_i = W_list[l] - W_l
-            r_i = -torch.reshape(r_list[l], [len(r_list[l]), 1]) + r_l
+def RoA(prog:MathematicalProgram, x, 
+        S:dict=None, W_B:dict=None, r_B:dict=None) -> MathematicalProgram:
+    # check if S is provided
+    if S is None:
+        # check if W_B, r_B are provided
+        if W_B is None or r_B is None:
+            raise ValueError("Activation set S or (W_B, r_B) are not provided")
+    else:
+        # if not, compute the linear expression of the output of the ReLU NN
+        W_B, r_B, _, _ = LinearExp(S)
+        
+    assert len(W_B) == len(r_B), "W_B and r_B must have the same amount of layers"
+    # stack W_B and r_B constraints
+    for keys, layer_info in W_B.items():
+        if keys == 0:
+            cons_W = layer_info
+            cons_r = r_B[keys]
         else:
-            W_pre = W_list[l] @ W_l
-            r_pre = W_list[l] @ r_l + r_list[l].reshape([len(r_list[l]), 1])
-            W_l = activated_set[num_neuron*l:num_neuron*(l+1)]*W_pre
-            r_l = activated_set[num_neuron*l:num_neuron*(l+1)]*r_pre
-            W_a = torch.vstack([W_a, W_l])
-            r_a = torch.vstack([r_a, r_l])
-            W_i = torch.vstack([W_i, W_pre - W_l])
-            r_i = torch.vstack([r_i, -torch.reshape(r_pre, [len(r_pre), 1]) + r_l])
-        B_act = [W_a, r_a]  # W_a x <= r_a
-        B_inact = [W_i, r_i]  # W_a x <= r_a
-    # W_overl = torch.matmul(W_list[-1], torch.matmul(W_list[-2], W_l))  # compute \overline{W}(S)
-    # r_overl = torch.matmul(W_list[-1], torch.matmul(W_list[-2], r_l) + r_list[-2].reshape([num_neuron,1])) + r_list[-1]  # compute \overline{r}(S)
-    W_overl = torch.matmul(W_list[-1], W_l)  # compute \overline{W}(S)
-    r_overl = torch.matmul(W_list[-1], r_l) + r_list[-1]  # compute \overline{r}(S)
-    return W_overl, r_overl, B_act, B_inact
+            cons_W = np.vstack([cons_W, layer_info])
+            cons_r = np.hstack([cons_r, r_B[keys]])
+    
+    # Add linear constraints to the MathematicalProgram
+    linear_constraint = prog.AddLinearConstraint(A= cons_W, lb=-cons_r, 
+                                                 ub=np.inf*np.ones(len(cons_r)), vars=x)
+    print(linear_constraint)
+    return prog
 
 # Given a activation set $S$, return the linear expression of the output of the ReLU NN
 def LinearExp(S:dict) -> (dict, dict, dict, dict):
@@ -95,20 +72,20 @@ def LinearExp(S:dict) -> (dict, dict, dict, dict):
         r_B_layer = np.multiply(layer_act_output_array, r_list[keys])
         # add boundary condition to W_B and r_B
         if keys == 0:
-            W_B[keys] = W_B_layer
-            r_B[keys] = r_B_layer
-            W_o[keys] = W_o_layer
-            r_o[keys] = r_o_layer
+            W_B[keys] = np.array(W_B_layer)
+            r_B[keys] = np.array(r_B_layer)
+            W_o[keys] = np.array(W_o_layer)
+            r_o[keys] = np.array(r_o_layer)
         else:
             W_o[keys] = np.matmul(W_o_layer, W_o[keys-1])
-            r_o[keys] = np.matmul(W_o_layer, r_o[keys-1]) + r_o_layer
+            r_o[keys] = np.matmul(W_o_layer, r_o[keys-1]) + np.array(r_o_layer)
             
             W_B[keys] = np.matmul(W_B_layer, W_o[keys-1])
-            r_B[keys] = np.matmul(W_B_layer, r_o[keys-1]) + r_B_layer
+            r_B[keys] = np.matmul(W_B_layer, r_o[keys-1]) + np.array(r_B_layer)
         
     return W_B, r_B, W_o, r_o
 
-def solver_lp():
+def solver_lp(model, S):
     # Input: X: Linear expression of the output of the ReLU NN
     # Output: X: Linear expression of the output of the ReLU NN
     # Create an empty MathematicalProgram named prog (with no decision variables,
@@ -116,19 +93,27 @@ def solver_lp():
     prog = MathematicalProgram()
     # Add two decision variables x[0], x[1].
     x = prog.NewContinuousVariables(2, "x")
-
-    # Add a symbolic linear expression as the cost.
-    cost1 = prog.AddLinearCost(x[0] + 3 * x[1] + 2)
-    # Print the newly added cost
-    print(cost1)
-    # The newly added cost is stored in prog.linear_costs().
-    print(prog.linear_costs()[0])
+    prog = RoA(prog, x, S)
+    # Add linear constraints
+    W_B, r_B, W_o, r_o = LinearExp(S)
+    print(W_B, r_B, W_o, r_o)
     
+    # Output layer index
+    index_o = len(S.keys())-1
+    # Add linear constraints
+    prog.AddLinearConstraint(np.array(W_o[index_o]).flatten() @ x + np.array(r_o[index_o]) == 0)
+    # Add cost function
+    # QC = prog.AddQuadraticCost(np.array(W_o[index_o]).flatten() @ x + np.array(r_o[index_o]))
+    
+    # Now solve the program.
+    result = Solve(prog)
+    print(f"Is solved successfully: {result.is_success()}")
+    print(f"x optimal value: {result.GetSolution(x)}")
+    print(f"optimal cost: {result.get_optimal_cost()}") 
     
 if __name__ == "__main__":
-    solver_lp()
 
-    architecture = [('relu', 2), ('relu', 32), ('linear', 1)]
+    architecture = [('relu', 2), ('relu', 32), ('relu', 32), ('linear', 1)]
     model = NNet(architecture)
     NStatus = NetworkStatus(model)
 
@@ -138,5 +123,4 @@ if __name__ == "__main__":
     x = random_input
     NStatus.get_netstatus_from_input(x)
     S = NStatus.network_status_values
-    W_B, r_B, W_o, r_o = LinearExp(S)
-    print(W_B, r_B, W_o, r_o)
+    solver_lp(model, S)
