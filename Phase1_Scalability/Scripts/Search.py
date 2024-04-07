22dde81f5fad37da3caa9fafc3e1c4c2d18d366d
Hongchao <hongchao@wustl.com>
2024-04-06 23:22:35 -0500

    Fix commented out code and enable bounding box constraint

--- Phase1_Scalability/Scripts/Search.py
+++ Phase1_Scalability/Scripts/Search.py
@@ -40,18 +40,18 @@ class Search(SearchInit):
         # Add linear constraints
         W_B, r_B, W_o, r_o = LinearExp(self.model, S)
         
-        prog = RoA(prog, x, model, S=None, W_B=W_B, r_B=r_B)
+        # prog = RoA(prog, x, model, S=None, W_B=W_B, r_B=r_B)
         # output constraints
         index_o = len(S.keys())-1
         prog.AddLinearEqualityConstraint(np.array(W_o[index_o]), np.array(r_o[index_o]), x)
         
-        # lb, ub = self.BoundingBox_approxi(S)
+        lb, ub = self.BoundingBox_approxi(S)
         neibour_neurons = {}
         for i in range(len(W_B)):
             neuron_list_layer = []
             for j in range(len(W_B[i])):
                 eq_cons = prog.AddLinearEqualityConstraint(np.array(W_B[i][j]), np.array(r_B[i][j]), x)
-                # bounding_box = prog.AddBoundingBoxConstraint(lb, ub, x)
+                bounding_box = prog.AddBoundingBoxConstraint(lb, ub, x)
                 result = Solve(prog)
                 
                 if result.is_success():
