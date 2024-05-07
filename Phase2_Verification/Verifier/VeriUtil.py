81bd1d808d81cbc485daf422a833be64942af543
Hongchao <hongchao@wustl.com>
2024-05-07 17:37:29 -0500

    Refactor code to add veri_seg_basic and veri_hinge_basic classes in VeriUtil.py

--- Phase2_Verification/Verifier/VeriUtil.py
+++ Phase2_Verification/Verifier/VeriUtil.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import numpy as np
 import torch
 import sys, os
@@ -5,19 +6,58 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from pydrake.solvers import MathematicalProgram, Solve
 from Modules.Function import RoA, LinearExp, solver_lp
 
+class veri_seg_basic():
+    def __init__(self, model, Case, S):
+        self.model = model
+        self.S = S
+        self.Case = Case
+        self.dim = Case.DIM
+        self._update_linear_exp(self.S)
+        
+    def _update_linear_exp(self, S):
+        self.W_B, self.r_B, self.W_o, self.r_o = LinearExp(self.model, S)
+        self.index_o = len(S.keys())-1
+        self.W_out = np.array(self.W_o[self.index_o]).flatten()
+        self.r_out = np.array(self.r_o[self.index_o])
+        
+    def XS(self, prog, x, S=None):
+        if S == None:
+            S = self.S
+        prog = RoA(prog, x, self.model, S)
+        return prog
+        
+class veri_hinge_basic():
+    def __init__(self, model, Case, S_list):
+        self.model = model
+        self.Case = Case
+        self.S_list = S_list
+        self.dim = Case.DIM
+        self._init_segs()
+        
+    def _init_segs(self):
+        self.segs = []
+        for S in self.S_list:
+            self.segs.append(veri_seg_basic(self.model, self.Case, S))
+
+    def XSi(self, prog, x, i):
+        return self.segs[i].XS(prog, x)
+    
+    def XS(self, prog, x):
+        for i in range(len(self.segs)):
+            prog = self.XSi(prog, x, i)
+        return prog
+
 class verifier_basic():
     def __init__(self, model, Case):
         self.model = model
         self.Case = Case
         self.dim = Case.DIM
-        self.x = None
-        self.W_B = None
-        self.r_B = None
-        self.W_o = None
-        self.r_o = None
-        self.index_o = None
-        self.Lfb = None
-        self.Lgb = None
-        self.no_control_flag = None
-        self.result = None
-        self.prog
+        # self.is_gx_linear = Case.linear_gx
+        self.is_gx_linear = True
+        # self.is_fx_linear = Case.linear_fx
+        self.is_fx_linear = True
+        # self.is_u_cons = Case.U_cons
+        self.is_u_cons = True
+        # self.is_u_cons_interval = Case.U_cons_interval
+        self.is_u_cons_interval = True
+        
