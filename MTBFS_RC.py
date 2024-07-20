f3e37231b909c19c433e25cc8f80940624d2681a
Hongchao <hongchao@wustl.com>
2024-07-20 13:48:42 -0500

    usage improved

--- MTBFS_RC.py
+++ MTBFS_RC.py
@@ -6,95 +6,88 @@ from SearchVerifier import SearchVerifier
 from Scripts.Search_MT import *
 import multiprocessing
 import time
+import os
+from RaycastInitSearch import *
+from Cases.ObsAvoid import ObsAvoid
 
-class SearchMT(Search):
+class SearchMTRC(Search):
     def __init__(self, model, case=None) -> None:
         super().__init__(model, case)
         self.model = model
         self.case = case
         self.NStatus = NetworkStatus(model)
         self.verbose = False
-        
+
         self.manager = multiprocessing.Manager()
         self.task_queue = self.manager.Queue()
         self.output_queue = self.manager.Queue()
-        self.boundary_dict = self.manager.list()
+        self.boundary_dict = self.manager.dict()
         self.boundary_list = self.manager.list()
         self.pair_wise_hinge = self.manager.list()
         self.num_workers = multiprocessing.cpu_count()
+        
+        self.Origin = [np.array([0.0, 0.0, 0.0])]
+        self.Direction = [np.array([0.0, 0.0, 1.0])]
+        
+        self.RCS = RaycastSearch(model, case, same_origin=True, same_direction=False)
+        self.RCS.num_rays = 10
+        self.RCS.raycast(self.Origin, self.Direction)
 
     def worker(self):
-        try:
-            while True:
-                if self.task_queue.empty():
-                    print("Worker received termination signal.")
-                    self.task_queue.put(None)
-                    break
-                current_set = self.task_queue.get()
-                # if self.task_queue.qsize()==0:
-                #     self.task_queue.put(None)
-                if current_set is None:
-                    print("Worker received termination signal.")
-                    break
-                
+        while True:
+            print('num of tasks:', self.task_queue.qsize())
+            current_set = self.task_queue.get()
+            if self.task_queue.empty():
+                break
+            if current_set is None:
+                print(f"Worker {os.getpid()} received termination signal.")
+                break
+            try:
                 res = solver_lp(self.model, current_set, SSpace=self.case.SSpace)
                 if res.is_success():
                     self.output_queue.put(current_set)
                     hashable_d = tuple(sorted((k, tuple(v)) for k, v in current_set.items()))
                     if hashable_d not in self.boundary_dict:
-                        self.boundary_dict.append(hashable_d)
+                        self.boundary_dict[hashable_d] = True
                         self.boundary_list.append(current_set)
 
                         unstable_neighbours = self.Filter_S_neighbour(current_set)
                         unstable_neighbours_S = self.Possible_S(current_set, unstable_neighbours)
-                        # print(f"Neighbours count: {len(unstable_neighbours_S)}")
-                        print('Task Queue Size:', self.task_queue.qsize())
 
                         for item in unstable_neighbours_S:
                             hashable_u = tuple(sorted((k, tuple(v)) for k, v in item.items()))
                             if hashable_u not in self.boundary_dict:
                                 self.task_queue.put(item)
-                    
-        except Exception as e:
-            print(f"Worker error: {e}")
-            return
-        finally:
-            print("Worker terminated.")
-            return
+            except Exception as e:
+                print(f"Worker error: {e}")
 
-    def BFS_parallel(self, root_node):
-        
-
-        init_queue, self.pair_wise_hinge = self.BFS(root_node, termination=2*self.num_workers)
-        for item in init_queue:
+    def BFS_parallel(self):
+        print("Initializing BFS_parallel...")
+        # init_queue, self.pair_wise_hinge = self.BFS(root_node, termination=2*self.num_workers)
+        for item in self.RCS.list_activation_intersections:
             self.task_queue.put(item)
+        print(f"Initial task queue size: {self.task_queue.qsize()}")
 
         workers = [multiprocessing.Process(target=self.worker) for _ in range(self.num_workers)]
 
         for w in workers:
             w.start()
-
-        # while any(p.is_alive() for p in workers):
-        #     try:
-        #         while not self.output_queue.empty():
-        #             current_set = self.output_queue.get()
-        #             # print(f"Processed: {current_set}")
-        #     except multiprocessing.queues.Empty:
-        #         continue
-
-        # Add sentinel values to stop the workers
-        # for _ in range(self.num_workers):
-        #     self.task_queue.put(None)
-
+            
         # Wait for all workers to finish
         for w in workers:
             w.join()
-            
+
+        # Add sentinel values to stop the workers
+        if self.task_queue.empty():
+            print("Worker received termination signal.")
+            self.task_queue.put(None)
+        
         for w in workers:
             w.terminate()
 
         print(f"Boundary list size: {len(self.boundary_list)}, Pair-wise hinge size: {len(self.pair_wise_hinge)}")
         return list(self.boundary_list), list(self.pair_wise_hinge)
+
     
 if __name__ == "__main__":
     # CBF Verification
@@ -110,13 +103,12 @@ if __name__ == "__main__":
     trained_state_dict = {f"layers.{key}": value for key, value in trained_state_dict.items()}
     model.load_state_dict(trained_state_dict, strict=True)
     
-    spt = torch.tensor([[[-1.0, 0.0, 0.0]]])
-    uspt = torch.tensor([[[0.0, 0.0, 0.0]]])
     # Search Verification and output Counter Example
     start_time = time.time()
-    Search_prog = SearchMT(model, case)
-    Search_prog.Specify_point(spt, uspt)
-    Search_prog.BFS_parallel(Search_prog.S_init[0])
+    Search_prog = SearchMTRC(model, case)
+    # Search_prog.Specify_point(spt, uspt)
+    # Search_prog.RCS.raycast(Origin, Direction)
+    Search_prog.BFS_parallel()
     end_time = time.time() - start_time
     print('Time taken:', end_time)
     # veri_flag, ce = Search_prog.SV_CE(spt, uspt)
